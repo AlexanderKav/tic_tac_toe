00e358a1da67c39fadae7a03b1153e3384fadb01
Alex Kavanagh <144698674+AlexanderKav@users.noreply.github.com>
2026-01-24 13:54:46 +0100

    Update player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,21 +1,36 @@
 class Player
-  attr_reader :name
-  def initialize(name)
-    @name = name
-  end
+    attr_reader :name
+    @@players_created = 0
+    def initialize(name)
+        @name = name
+        @@players_created +=1
+        @player_mark = player_marker
+    end
 
- # def name
-  #  @name
-  #end
-def choose_option()
-  puts "Enter the row: "
-  row = gets.chomp
-  row = row.to_i
+    def choose_option()
+        position_choice = []
+        
+        print "Enter the row: "
+        row = gets.chomp
+        row = row.to_i
+        position_choice.push(row)
 
-  puts "Enter the column: "
-  column = gets.chomp
-  column = row.to_i
-end
+        print "Enter the column: "
+        column = gets.chomp
+        column = column.to_i
+        position_choice.push(column)
+    end
 
+    def player_marker()
+        if @@players_created % 2 == 0
+            @player_mark = "O"
+        else
+            @player_mark = "X"
+        end
+        @player_mark
+    end
 
+    def set_mark()
+        @player_mark
+    end
 end
