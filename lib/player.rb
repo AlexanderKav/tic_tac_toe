c7d3b2e30d36cd3c84d446a38d8bc34f94ec8a37
Alex Kavanagh <144698674+AlexanderKav@users.noreply.github.com>
2026-01-24 16:32:56 +0100

    Update player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -9,15 +9,16 @@ class Player
 
     def choose_option()
         position_choice = []
-        
         print "Enter the row: "
         row = gets.chomp
         row = row.to_i
+        row = valid_input?(row)
         position_choice.push(row)
 
         print "Enter the column: "
         column = gets.chomp
         column = column.to_i
+        column = valid_input?(column)
         position_choice.push(column)
     end
 
@@ -33,4 +34,19 @@ class Player
     def set_mark()
         @player_mark
     end
+
+    def valid_input?(number)
+        loop do
+            if number > 3 || number < 1
+                print "Invalid Input! "
+                print "Enter a number between 1 and 3: "
+                number = gets.chomp
+                number = number.to_i
+            else
+                return number
+            end
+            
+        end
+    end
+
 end
