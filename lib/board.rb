4f0ec2eccdc2aad3da970daaa4e465df9bb78e7b
Alex Kavanagh <144698674+AlexanderKav@users.noreply.github.com>
2026-01-24 13:54:23 +0100

    Update board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,42 +1,61 @@
 class Board
-  #@@board_layout = 'Hello'
-  #def intialize()
-  #  @@board_layout = [
-   #   '_','|_','|_',
-    #  '_','|_','|_',
-     # ' ','| ','| ' 
-    #]
-
-    @@board_layout = "_|_|_\n_|_|_\n | | "
-     
-    #@board_layout = [1,2,3,3]
-
-  #end
-
+    @@board_layout =[ 
+    ["_", "_", "_"],
+    ["_", "_", "_"],
+    [" ", " ", " "],
+]
 
   def self.board_layout
-   # for parts in @board_layout do
-    #  puts parts
-    #end
     @@board_layout
   end
 
-  def self.convert_to_array
-    #@@board_layout.split("|")
-    #puts @@board_layout.split("|").inspect
-   puts @@board_layout.split("\n").inspect
-   b = @@board_layout.split("\n")
-    #puts @@b.inspect
-   #b[0][0] = "X\u0331"
-   #b[0][3] = "X\u0331"
-   #b[0][6] = "X\u0331"
-   #b[1][4] = "O\u0331"
-   puts b
+  def self.convert_to_string
+       @@board_layout.each_with_index.map do |group, i|
+            group.join("|")
+        end.join("\n")
   end
 
- # def print_board
-  #  puts @board_layout.join(" | ")
-  #end
+  def self.check_if_available?(row, column)
+    if @@board_layout[row][column] == "X" || 
+        @@board_layout[row][column] == "O"
+        puts "Space already taken"
+        return true
+    else 
+        return false
+    end
+  end
 
+  def self.game_over?(mark, player_name)
+    case    
+        when 
+            @@board_layout[0][0] == mark && @@board_layout[0][1]== mark && @@board_layout[0][2] == mark
+                || 
+            @@board_layout[1][0] == mark && @@board_layout[1][1] == mark && @@board_layout[1][2] == mark
+                ||
+            @@board_layout[2][0]== mark && @@board_layout[2][1]== mark && @@board_layout[2][2]== mark
+                ||
+
+            @@board_layout[0][0]== mark && @@board_layout[1][0]== mark && @@board_layout[2][0]== mark
+                ||
+            @@board_layout[0][1]== mark && @@board_layout[1][1]== mark && @@board_layout[2][1]== mark
+                ||
+            @@board_layout[0][2]== mark && @@board_layout[1][2] == mark && @@board_layout[2][2]== mark
+                ||
+
+            @@board_layout[0][0] == mark && @@board_layout[1][1]== mark && @@board_layout[2][2]== mark
+                ||
+            @@board_layout[0][2] == mark && @@board_layout[1][1]== mark && @@board_layout[2][0]== mark
+                puts player_name + " won!!!"  
+                puts "Game Over"
+                true
+        else 
+            false
+        end
+    end
+
+
+    def self.is_draw?
+        @@board_layout.flatten.all? { |mark| mark == "X" || mark == "O"}
+    end
 
 end
